c96d3c54d76992b747de8dc1304b3929222d8d5a
Justin Harrison <justin@matthin.com>
2015-07-15 13:46:49 -0400

    Partial spree homepage modification

--- app/helpers/spree_helper.rb
+++ app/helpers/spree_helper.rb
@@ -1,10 +1,15 @@
 module SpreeHelper
   def content_class
     if content_for?(:sidebar)
-      'col-sm-8 col-md-9'
+      'col-sm-4 col-md-5'
     else
-      'col-sm-12'
+      'col-sm-9'
     end
+  end
+
+  def product_details(product)
+    brand = product.taxons.select { |taxon| taxon.name == "Brand" }
+    "#{brand} - #{product.id}-#{product.price}"
   end
 end
 
